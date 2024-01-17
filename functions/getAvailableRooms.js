4f0a098a3470ae41f0c303700a0430f7574fe6b2
alhassan50 <abdullahialhassanbaaba@example.com>
2024-01-17 14:34:58 +0000

    fixed cors header issue

--- functions/getAvailableRooms.js
+++ functions/getAvailableRooms.js
@@ -7,7 +7,7 @@ const validateAvailableRoomsParams = require("../utility/validateAvailableRoomsP
 const availableRooms = require("../utility/availableRooms")
 
 const corsHeaderOptions = {
-    /* 'Access-Control-Allow-Origin': 'http://localhost:3000', */
+    //'Access-Control-Allow-Origin': 'http://localhost:3000',
     'Access-Control-Allow-Origin': 'https://platinumhostels.vercel.app',
     'Access-Control-Allow-Methods': 'GET, OPTIONS',
     'Access-Control-Allow-Headers': 'Content-Type, Authorization',
@@ -44,6 +44,8 @@ exports.handler = async (event) => {
 
         // Use query parameters in your logic
         const availableRoomsArray = await availableRooms(hostelLocation, roomType, gender);
+
+        console.log(availableRoomsArray);
         
         return {
             statusCode: 200,
