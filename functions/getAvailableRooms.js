9d72bc2d38dacaa1637eb67bf29ab5ef11bc55d5
alhassan50 <abdullahialhassanbaaba@gmail.com>
2024-01-22 16:58:21 +0000

    changed allowed origins from dev origins to prod origins

--- functions/getAvailableRooms.js
+++ functions/getAvailableRooms.js
@@ -6,7 +6,6 @@ const availableRooms = require("../utility/availableRooms")
 const corsHeaderOptions = {
     'Access-Control-Allow-Origin': 'https://platinumhostels.vercel.app',
     //'Access-Control-Allow-Origin': 'http://localhost:3000',
-    //'Access-Control-Allow-Origin': ['http://localhost:3000', 'https://platinumhostels.vercel.app'],
     'Access-Control-Allow-Methods': 'GET, OPTIONS',
     'Access-Control-Allow-Headers': 'Content-Type, Authorization',
 };
